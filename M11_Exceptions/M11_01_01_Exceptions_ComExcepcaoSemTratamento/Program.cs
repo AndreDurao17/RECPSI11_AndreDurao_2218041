456ce27a23b2e6beb354610b89143d4ba987f511
AndreDurao17 <58223458+AndreDurao17@users.noreply.github.com>
2021-03-10 16:03:57 +0000

    M11_Exceptions

---
+++ M11_Exceptions/M11_01_01_Exceptions_ComExcepcaoSemTratamento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace M11_01_01_Exceptions_ComExcepcaoSemTratamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exemplo = "123ab";
+            int teste = Int32.Parse(exemplo);
+            Console.WriteLine($"Resultado da conversão: {teste}");
+
+            Console.ReadKey();
+
+           /* a - O código deu erro
+            * b - No exemplo "123ab" tem letras o que não funciona,
+            * pois o int so funciona para numeros 
+            * c - Ha uma excepção apresentada que não está a ser tratada 
+           */
+        }
+    }
+}
